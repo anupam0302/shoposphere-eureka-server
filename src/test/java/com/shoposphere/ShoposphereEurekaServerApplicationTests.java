d021cd9e4a2bf4b98dccb4ea95cc3d10df0fde08
Anupam Anand <67553013+anupam0302@users.noreply.github.com>
2025-11-16 19:56:46 +0530

    Added config for load balancing

---
+++ src/test/java/com/shoposphere/ShoposphereEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoposphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoposphereEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
